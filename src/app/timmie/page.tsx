fdf24ab30020e7a098ee0fbbc9d2ac1f583eadb8
Daan Bouma <130249739+DaanBouma@users.noreply.github.com>
2026-01-10 19:09:34 +0100

    -

--- src/app/timmie/page.tsx
+++ src/app/timmie/page.tsx
@@ -1,12 +1,10 @@
-import React from 'react'
-import ReactPlayer from 'react-player'
 import "./style.css"
 
 export default function Page() {
   return (
     <>
       <div className="fixed">
-        <ReactPlayer style={{ height: "70vh", width: "auto" }} controls src='/beerchallenge.mp4' />
+        <video style={{ height: "70vh", width: "auto" }} controls src='/beerchallenge.mp4' />
       </div>
     </>
   )
